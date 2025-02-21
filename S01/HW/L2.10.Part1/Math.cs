8f9f53bf5a4b2c1916c506bb418eea0942e6befe
unknown <aliebanikhazaei@gmail.com>
2025-02-21 17:39:04 +0330

    Vance

---
+++ S01/HW/L2.10.Part1/Math.cs
@@ -0,0 +1,42 @@
+﻿namespace L2._10.Part1
+{
+    public class Program
+    {
+        public static int Factorial(int n)
+        {
+            if(n==1 || n==0)
+                return 1 ;
+
+            int i = 1 ;
+            int s = 1 ;
+            while(i<n+1)
+            {
+                s *= i ;
+                i++ ;
+            }
+            return s ;
+        }
+
+        public static void E(double precision)
+        {
+            int a = 0 ;
+            double e = 0 ;
+            while(true)
+            {
+                e += Math.Pow(1,a)/Factorial(a) ;
+                a += 1 ;
+                if (Math.Pow(1,a)/Factorial(a) < precision)
+                    break ;
+            }
+            Console.Write(e);
+        }
+
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            E(x);
+        }
+    }
+}
+
+
---
+++ S01/HW/L2.9.Part2/Assert.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+namespace L2._9.Part2
+{
+    public class Program
+    {
+        public static bool IsPrime(int a)
+        {
+            Debug.Assert(a>0);
+
+            if(a==1)
+                return false ;
+
+            if(a==2)
+                return true ;
+
+            for(int i=2 ; i<a ; i++)
+            {
+                if(a%i == 0)
+                    return false ;
+            }
+            return true ;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(IsPrime(x));
+        }
+    }
+}
+
+
