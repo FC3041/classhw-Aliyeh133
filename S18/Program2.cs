b667e06df56bcfbf7f3bd75ea9a663e66f922bdd
unknown <aliebanikhazaei@gmail.com>
2025-05-01 14:34:10 +0330

    delegate

--- S18/Program2.cs
+++ S18/Program2.cs
@@ -5,21 +5,35 @@ class Program2
     delegate T _tow_number<T>(T a , T b);
     public static int add(int x , int y) => x + y ;
     public static double divide(double x , double y) => x/y ;
-    static void apply<T>(T[] nums0 , T[] nums1 , T[] result , _tow_number<T> fn)
+    // static void apply<T>(T[] nums0 , T[] nums1 , T[] result , _tow_number<T> fn)
+    // {
+    //     for(int i=0 ; i<nums0.Length ; i++)
+    //         result[i] = fn(nums0[i] , nums1[i]);
+    // }
+
+    static void apply<T>(T[] nums0 , T[] nums1 , T[] result , Func<T,T,T> fn)
     {
         for(int i=0 ; i<nums0.Length ; i++)
             result[i] = fn(nums0[i] , nums1[i]);
     }
+
+    static void dodo<T>(T[] values , Action<T> a)
+    {
+        foreach(var v in values)
+            a(v) ;
+    }
+
     static void Main(string[] args)
     {
-        double[] nums0 = new double[3]{3,5,8};
-        double[] nums1 = new double[3]{5,7,9};
-        double[] result = new double[3];
+        int[] nums0 = new int[3]{3,5,8};
+        int[] nums1 = new int[3]{5,7,9};
+        int[] result = new int[3];
 
-        apply(nums0,nums1,result,divide);
+        apply(nums0,nums1,result, (a,b) => a*b );
+        dodo(result,Console.WriteLine);
 
-        for(int i=0 ; i<3 ; i++)
-            Console.Write($"{result[i]} ");
+        // for(int i=0 ; i<3 ; i++)
+        //     Console.Write($"{result[i]} ");
         
     }
 
