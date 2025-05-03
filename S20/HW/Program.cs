95e8c6778aa1b0707c4a5a5d25ad11a591a183e1
unknown <aliebanikhazaei@gmail.com>
2025-05-04 01:29:33 +0330

    Encoder & Decoder

---
+++ S20/HW/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Specialized;
+
+namespace HW;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = "sihhYIOP" ;
+        string En_s = s.Encoder();
+        string De_s = En_s.Decoder();
+        Console.WriteLine(En_s);
+        Console.WriteLine(De_s);
+    }
+}
